aba5ceddf2ebd240707ba1f927533cc1e5173617
markstubb <mark@digitalpimp.org>
2013-12-13 12:38:43 -0500

    Update foot.php
    Put the version number of minepeon on every page.

--- http/foot.php
+++ http/foot.php
@@ -1,7 +1,8 @@
 <footer class="container">
   <hr />
   Server Time: <?php echo date('D, d M Y H:i:s T') ?>
-
+  <?php $version = file_get_contents('/opt/minepeon/etc/version'); ?>
+  <br>Version: <?=$version?>
   <?php if(empty($settings['donateAmount'])) { echo $plea; } ?>
 </footer>
 
