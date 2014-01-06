79e2a0809a3c56747cfe40bbb64d108a034cd766
mariogrip <mario.grip@gmail.com>
2014-01-07 00:18:52 +0100

    API for settings and pool

---
+++ http/plugins/testpl/settings.php
@@ -0,0 +1,18 @@
+<?php
+
+include('../../inc/settings.inc.php');
+
+
+
+include('../../head.php');
+include('../../menu.php');
+
+?>
+
+
+        <div class="container">
+<center>Test Plugin</center>
+        </div>
+<?php
+
+include('../../foot.php');
