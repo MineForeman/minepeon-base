15cca97ad35a857e750e60f37630701dfdf7e02c
Neil Fincham <neil@MineForeman.com>
2014-03-02 09:31:46 +1300

    Add promotePool for pool switching

--- http/inc/miner.inc.php
+++ http/inc/miner.inc.php
@@ -27,3 +27,16 @@ function miner($command, $parameter) {
 
 }
 
+function promotePool($addr, $user){
+  $pools = cgminer('pools','')['POOLS'];
+  $pool = 0;
+  // echo "changeing";
+  foreach ($pools as $key => $value) {
+    if(isset($value['User']) && $value['URL']==$addr && $value['User']==$user){
+	  // echo "found";
+	  cgminer('switchpool',$pool);
+    }
+	$pool = $pool + 1;
+  }
+  
+}
