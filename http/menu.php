88f015e7e659a4156ccd7e032ffd8dc305eb3bd1
mariogrip <mario.grip@gmail.com>
2014-03-11 01:40:01 +0100

    Added "beta" to navbar

--- http/menu.php
+++ http/menu.php
@@ -9,7 +9,7 @@ include("lang/en/lang.en.php");
   
 <div class="navbar navbar-default">
 <div class="container">
-    <a class="navbar-brand" href="http://mineforeman.com/minepeon/">MinePeon</a>
+    <a class="navbar-brand" href="http://mineforeman.com/minepeon/"><small>Beta </small>MinePeon</a>
     <ul class="nav navbar-nav">
       <li><a href="/"><?php echo $lang["status"]; ?></a></li>
       <li><a href="/pools.php"><?php echo $lang["pools"]; ?></a></li>
