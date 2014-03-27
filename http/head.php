93b1f75b350d21584e3a86fc225536bd32101292
Neil Fincham <neil@MineForeman.com>
2014-03-28 10:57:41 +1300

    Refresh / instead of a reaload
    http://minepeon.com/forums/viewtopic.php?p=6519#p6519

--- http/head.php
+++ http/head.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
   <meta name="author" content="">
-  <meta http-equiv="refresh" content="600">
+  <meta http-equiv="refresh" content="600;URL='/'" />
 
   <link href="/css/bootstrap.min.css" rel="stylesheet">
   <link href="/css/bootstrap-minepeon.css" rel="stylesheet">
